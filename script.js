37cea90d3116504f2e63e2ba891b78f761139f4d
BigFatDuck1 <clementho999@gmail.com>
2023-11-01 23:42:48 +0800

    Changes to a football when the basketball falls to the bottom

--- script.js
+++ script.js
@@ -20,6 +20,22 @@ document.addEventListener('scroll', function() {
     //If scrolling back up
     if (scroll_top < max_scrollTop) {
         document.querySelector(".ball").classList.remove("fall");
+        document.querySelector(".ball").textContent = "🏀";
     }
-
 });
+
+//Change to a football on transition end
+document.querySelector(".ball").addEventListener("transitionend", function() {
+    //scrollTop is the difference between top of content and top of container
+    //i.e. the gap between what cannot be seen and the height of the viewport
+    let scroll_top = Math.floor(document.documentElement.scrollTop);
+    //Viewport height
+    let viewport_height = document.querySelector("html").clientHeight;
+    //Content height
+    let content_height = document.querySelector("body").scrollHeight;
+
+    //This is necessary because otherwise on upwards transition end, the ball will also change to a football
+    if (scroll_top == content_height - viewport_height) {
+        document.querySelector(".ball").textContent = "⚽️";
+    }
+}); 
